aac2b7c8f7ff738f4b811a8939cc296a215ace45
NOX-0202 <fernandinho.nco@gmail.com>
2022-10-06 10:26:20 -0300

    inicial

---
+++ aula-04/primeiroPrograma/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // call foo
+        foo();
+    }
+
+    public static void foo() {
+        System.out.println("foo");
+    }
+
+    public static void bar() {
+        System.out.println("bar");
+    }
+}
